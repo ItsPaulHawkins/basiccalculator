0aaad92eae4c150a63d26306fcf893d58f0b8103
ItsPaulHawkins <hawkinsanthonypaul@gmail.com>
2017-02-07 16:59:04 -0500

    Finished calculator with remainders.

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,6 +10,72 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            bool loop = true;
+            while(loop == true){
+                Console.WriteLine("Would you like to add, subtract, multiply, or divide?");
+                string operation = Console.ReadLine();
+                Console.WriteLine("What is your first number?");
+                int first = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("What is your second number?");
+                int second = Int32.Parse(Console.ReadLine());
+                if (operation == "add" || operation == "+")
+                {
+                    int final = first + second;
+                    Console.WriteLine("Your final number is " + final + ". Would you like to go again?");
+                    string ifloop = Console.ReadLine();
+                    if(ifloop == "yes")
+                    {
+                        loop = true;
+                    }else
+                    {
+                        loop = false;
+                    }
+
+                }else if(operation == "subtract" || operation == "-")
+                {
+                    int final = first - second;
+                    Console.WriteLine("Your final number is " + final + ". Would you like to go again?");
+                    string ifloop = Console.ReadLine();
+                    if (ifloop == "yes")
+                    {
+                        loop = true;
+                    }else
+                    {
+                        loop = false;
+                    }
+                }else if(operation == "Multiply" || operation == "*")
+                {
+                    int final = first * second;
+                    Console.WriteLine("Your final number is " + final + ". Would you like to go again?");
+                    string ifloop = Console.ReadLine();
+                    if(ifloop == "Yes")
+                    {
+                        loop = true;
+                    }else
+                    {
+                        loop = false;
+                    }
+                }else if(operation == "divide" || operation == "/")
+                {
+                    int final = first / second;
+
+                    if(first%second == 0)
+                    {
+                        Console.WriteLine("Your final number is " + final + ". Would you like to go again?");
+                    }else
+                    {
+                        Console.WriteLine("Your final number is " + final + ", with a remainder of " + (first % second) + ". Would you like to go again?");
+                    }
+                    string ifloop = Console.ReadLine();
+                    if(ifloop == "yes")
+                    {
+                        loop = true;
+                    }else
+                    {
+                        loop = false;
+                    }
+                }
+            }
         }
     }
 }
