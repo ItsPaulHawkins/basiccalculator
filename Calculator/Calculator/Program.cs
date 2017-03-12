1c8c420b443955224165cb313d6ea87e6548922a
Paul Hawkins <hawkinsanthonypaul@gmail.com>
2017-03-12 17:49:14 -0500

    Implemented functions so my code is actually readable.

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -8,73 +8,75 @@ namespace Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        static private bool again = true;
+       static void Main(string[] args)
         {
-            bool loop = true;
-            while(loop == true){
-                Console.WriteLine("Would you like to add, subtract, multiply, or divide?");
-                string operation = Console.ReadLine();
+
+            while (again == true)
+            {
+                Console.WriteLine("Do you want to add, subtact, multiply, or divide?");
+                string answer = Console.ReadLine();
                 Console.WriteLine("What is your first number?");
                 int first = Int32.Parse(Console.ReadLine());
                 Console.WriteLine("What is your second number?");
                 int second = Int32.Parse(Console.ReadLine());
-                if (operation == "add" || operation == "+")
+                switch (answer)
                 {
-                    int final = first + second;
-                    Console.WriteLine("Your final number is " + final + ". Would you like to go again?");
-                    string ifloop = Console.ReadLine();
-                    if(ifloop == "yes")
-                    {
-                        loop = true;
-                    }else
-                    {
-                        loop = false;
-                    }
-
-                }else if(operation == "subtract" || operation == "-")
-                {
-                    int final = first - second;
-                    Console.WriteLine("Your final number is " + final + ". Would you like to go again?");
-                    string ifloop = Console.ReadLine();
-                    if (ifloop == "yes")
-                    {
-                        loop = true;
-                    }else
-                    {
-                        loop = false;
-                    }
-                }else if(operation == "multiply" || operation == "*")
-                {
-                    int final = first * second;
-                    Console.WriteLine("Your final number is " + final + ". Would you like to go again?");
-                    string ifloop = Console.ReadLine();
-                    if(ifloop == "Yes")
-                    {
-                        loop = true;
-                    }else
-                    {
-                        loop = false;
-                    }
-                }else if(operation == "divide" || operation == "/")
-                {
-                    int final = first / second;
-
-                    if(first%second == 0)
-                    {
-                        Console.WriteLine("Your final number is " + final + ". Would you like to go again?");
-                    }else
-                    {
-                        Console.WriteLine("Your final number is " + final + ", with a remainder of " + (first % second) + ". Would you like to go again?");
-                    }
-                    string ifloop = Console.ReadLine();
-                    if(ifloop == "yes")
-                    {
-                        loop = true;
-                    }else
-                    {
-                        loop = false;
-                    }
+                    case "add":
+                        add(first, second);
+                        break;
+                    case "subtract":
+                        subtract(first, second);
+                        break;
+                    case "divide":
+                        divide(first, second);
+                        break;
+                    case "multiply":
+                        multiply(first, second);
+                        break;
+                    default:
+                        Console.WriteLine("That is not a correct operation :/");
+                        Console.ReadLine();
+                        break;
+                        
                 }
+                Console.WriteLine("Would you like to go again?");
+                string askAgain = Console.ReadLine();
+                againMethod(askAgain);
+                
+            }
+        }
+        public static void add(int num1, int num2)
+        {
+            Console.WriteLine("Your final number is " + (num1 + num2));
+        }
+        public static void subtract(int num1, int num2)
+        {
+            Console.WriteLine("Your final number is " + (num1 - num2));
+        }
+        public static void multiply(int num1, int num2)
+        {
+            Console.WriteLine("Your final number is " + (num1 * num2));
+        }
+        public static void divide(int num1, int num2)
+        {
+            if(num1%num2 != 0)
+            {
+                Console.WriteLine("Your final number is " + (num1 / num2) + " with a remainder of " + (num1 % num2));
+            }else
+            {
+                Console.WriteLine("Your final number is " + (num1 / num2));
+            }
+        }
+        public static void againMethod(string str1)
+        {
+            
+            if(str1 == "yes")
+            {
+                again = true;
+            }else
+            {
+                again = false;
             }
         }
     }
